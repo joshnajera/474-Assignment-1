85da7e8eaabc02ce57da52f793efd29781ffcd15
switch620 <switch620@gmail.com>
2017-10-06 15:36:46 -0700

    Cleaning up code

--- assignment1.py
+++ assignment1.py
@@ -1,48 +1,53 @@
-import _operator
 import  copy
+import _operator
 
-    
-class myGen:
-    process_positions = []      # Holds processes involved with each Logical Clock Time Stamp
-    processes = []              # Holds input data stored as list
-    result = []                 # Holds results
-    num_processes = 0           # Holds number of processes
-    max = 0                     # Holds the maximum Logical Clock Time Stamp value
-    send_receive_count = 0
-    
+
+class MyGen:
+    '''Processes input file of logical clock values and generates events that correspond'''
     def __init__(self):
+        self.process_positions = []      # Holds processes involved with each Logical Time
+        self.processes = []              # Holds input data stored as list
+        self.result = []                 # Holds results
+        self.num_processes = 0           # Holds number of processes
+        self.send_receive_count = 0
+        self.max_clock_value = 0         # Holds the maximum Logical Clock Time Stamp value
+
         with open("input.txt") as in_file:
+
+           # Read input, populate 'processes', get the process count, size 'result' accordingly
             for line in in_file:
-                self.processes.append([int(i) for i in line.strip().split(' ')])    # Load each row in the file into 'processes'
+                self.processes.append([int(i) for i in line.strip().split(' ')])
                 self.num_processes += 1
-                self.result.append([])  # Setting up result to have as many rows as processes
-            
-            for i, process in zip(range(self.num_processes), self.processes):       # Goes through 'process'...
+                self.result.append([])
                 self.process_positions.append(0)    # Appending zeros to pad
-                
-                for j in range(len(process)):
-                    if process[j] > self.max:       # Finding 'max' logical clock value
-                        self.max = process[j]
-            
-            self.event_times = [set() for i in range(self.max)]     # Initializing 'event_times' size based off 'max' logical clock value
-            for process in range(self.num_processes):               # Populating with data from 'processes'
-                for event in self.processes[process]:
-                    if event == 0:
-                        continue
-                    self.event_times[event - 1].add(process)
-                    
-        for event in self.event_times:
-            print(event)
-        
-        self.generatePossibilities(self.result)     # Calls function to evaluate input
-                    
-    def generatePossibilities(self, result, flag = 0, pos = 0):
-        event = self.event_times[pos]   # Getting current list of processes involved with events at current logical clock time value
-        output = copy.deepcopy(result)  # Making a copy of output ... (Copies made so we don't affect original if we want to recursively find all possible solutions)
-        
-        print("\nIteration: {}".format(pos+1))
-        if flag == 0 and pos < len(self.event_times) -1:    # If Send flag is not set and this isn't the last item
-                                                            # This indicates that this wont be a receive
+
+            for process in self.processes:       # Goes through 'process'...
+                self.max_clock_value = max(max(iter(process)), self.max_clock_value)
+
+            # Initializing 'event_times' size based off 'max_clock_value' logical clock value
+            self.event_times = [set() for i in range(self.max_clock_value)]
+
+            # Populate 'event_times' with data from 'processes'
+            for process in enumerate(self.processes):
+                for event in process[1]:
+                    if event != 0:
+                        self.event_times[event - 1].add(process[0])
+
+        # for event in self.event_times:
+        #     print(event)
+
+        self.generate_possibilities(self.result)     # Calls function to evaluate input
+
+
+    def generate_possibilities(self, working_result, s_flag=0, pos=0):
+        '''Uses 'self.event_times' to determine the events that occurred at each logical time'''
+        event = self.event_times[pos]   # Getting current list of processes
+        output = copy.deepcopy(working_result)  # Making a copy of working_result...
+
+        # print("\nIteration: {}".format(pos+1))
+        # If Send s_flag is not set and this isn't the last item
+        if s_flag == 0 and pos < len(self.event_times) -1:    
+
             print("{Next} - {Current}= ", self.event_times[pos+1] - event)
             if self.event_times[pos+1] - event != set():    # If set difference Next-Current != Empty Set: Then there is a send event
                 print("Set at least one send, set others internal")
@@ -55,8 +60,8 @@ class myGen:
                             print("Make '{}' internal...".format(item_j))
                             output[item_j].append('i')
                     # print(output)
-                    self.generatePossibilities(output, flag+1, pos+1)   # Make next recursive call
-                # flag += 1
+                    self.generate_possibilities(output, s_flag+1, pos+1)   # Make next recursive call
+                # s_flag += 1
                 
             else:   # if the set difference Next-Current == Empty: Then there isn't a send event and all are internal
                 print("No send event this time")
@@ -65,9 +70,9 @@ class myGen:
                     print("Make '{}' internal...".format(item))
                     output[item].append('i')
                 # print(output)
-                self.generatePossibilities(output, flag, pos+1)
+                self.generate_possibilities(output, s_flag, pos+1)
                 
-        else:   # Else flag is raised   TODO Handle when we have receive AND send at same time
+        else:   # Else s_flag is raised   TODO Handle when we have receive AND send at same time
             possible_receives = event - self.event_times[pos-1]
             if possible_receives:                           # If set difference Current-next: then we have a receive
                 print("Set a receive event, others internal?")       # TODO: what if more than one send was made?
@@ -89,16 +94,16 @@ class myGen:
                                     if item_j != item_k:
                                         print("Make '{}' internal...".format(item_k))
                                         output[item_k].append('i')
-                        self.generatePossibilities(output, flag, pos+1)
+                        self.generate_possibilities(output, s_flag, pos+1)
                     
                     else:
                         for item_j in event - possible_receives:
                             print("Make '{}' internal...".format(item_j))
                             output[item_j].append('i')  # Set the rest to internal
-                        self.generatePossibilities(output, flag-1, pos+1)
+                        self.generate_possibilities(output, s_flag-1, pos+1)
                 
                 # print(output)
-                # flag -= 1
+                # s_flag -= 1
             else:
                 print("Last element? No receives left? Must be internal")
                 for item in event:  # Catches the case where this is the last element?
@@ -113,4 +118,4 @@ class myGen:
     # print(i, event)
     
         
-asdf = myGen()
+asdf = MyGen()
